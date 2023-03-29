7bdf17a4817b038cd482a86d630a30a26586d33e
jeypow <ajeypow@gmail.com>
2023-03-29 14:31:54 -0300

    Master

--- lib/home.dart
+++ lib/home.dart
@@ -12,7 +12,44 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF234E98),
-      body: Container(),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.fromLTRB(20, 40, 20, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/logo.png', // substitua com o caminho da imagem do seu logo
+                height: 100,
+              ),
+              SizedBox(height: 180),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.blue,
+        child: PreferredSize(
+          preferredSize: Size.fromHeight(kToolbarHeight),
+          child: Container(
+            color: Colors.white,
+            height: kToolbarHeight,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.home),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.notifications),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
